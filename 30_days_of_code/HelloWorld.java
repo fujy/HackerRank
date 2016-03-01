f5d0ba9c51a5d47e8a177ac2099f312f2c71728e
fujy <fujy@example.com>
2016-03-01 15:40:52 +0100

    Day 0

---
+++ 30_days_of_code/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+       public static void main(String[] args) {
+        
+          // Write your print statements here!! 
+          // Template: System.out.println("what you want to print");
+           System.out.println("Hello World.");
+           System.out.println("Welcome to 30 Days of Code.");
+        
+       }
+   }
