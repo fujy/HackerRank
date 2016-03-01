4f0c46a7e2b6c806418829147325eb0d0cf4d53a
fujy <fujy@example.com>
2016-03-01 15:59:13 +0100

    Day 6

---
+++ 30_days_of_code/Review1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+        
+    }
+}
+
