e666e99a4a560136f0fff799f40de60ef008961c
fujy <fujy@example.com>
2016-03-01 16:10:28 +0100

    Day 7

---
+++ 30_days_of_code/Arrays.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int arr[] = new int[N];
+        for(int i=0; i < N; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int i=arr.length - 1; i >= 0 ; i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
+
