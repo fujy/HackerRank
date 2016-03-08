69fceab0e923716ee3c487001114baf1a5e52b75
fujy <fujy@example.com>
2016-03-08 15:36:38 +0100

    Day 27

---
+++ contests/30_days_of_code/Testing2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("5");
+        System.out.println("4 3");
+        System.out.println("-1 0 4 2");
+        System.out.println("5 3");
+        System.out.println("0 -1 -2 1 4");
+        System.out.println("3 3");
+        System.out.println("0 -1 1");
+        System.out.println("6 5");
+        System.out.println("0 -1 -2 -3 -4 2");
+        System.out.println("7 3");
+        System.out.println("0 -1 1 2 3 4 5");
+    }
+}
