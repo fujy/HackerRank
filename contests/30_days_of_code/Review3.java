92871e5226472a84e60056eec04b9d4ebd4d7d2a
fujy <fujy@example.com>
2016-03-08 15:33:54 +0100

    Day 20

---
+++ contests/30_days_of_code/Review3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringTokenizer st = new StringTokenizer(s," ![,?.\\_'@+]");
+        System.out.println(st.countTokens());
+        while (st.hasMoreTokens()) {
+         System.out.println(st.nextToken());
+        }
+    }
+}
